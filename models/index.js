7d489dab6517aea8cd8fa040711b5d7494d0d6c3
chrislose23 <chris@Christophers-MacBook-Air.local>
2024-05-22 16:13:08 -0400

    added Category model info

--- models/index.js
+++ models/index.js
@@ -7,6 +7,10 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
 
